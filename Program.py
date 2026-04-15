f981b270d3d6132750a66abc241e057b517b4c5d
nisim aharonov <nisimaharonov@gmail.com>
2026-04-15 11:18:02 +0300

    Add factorial function and tests

--- Program.py
+++ Program.py
@@ -1,2 +1,22 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+
+
+
+
+
+
+
+
+
 total_sum = sum(range(1, 100))
 print(total_sum)
+
+numbers = [5, 6, 7, 8]
+for num in numbers:
+    print(f"The factorial of {num} is: {factorial(num)}")
